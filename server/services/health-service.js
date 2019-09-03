89a72b6e01d16816f8b8d8b8928e2fb99ab2e107
John Watson <john.watson2@rpa.gov.uk>
2019-09-03 15:12:17 +0100

    Lambda file functions removed

--- server/services/health-service.js
+++ server/services/health-service.js
@@ -4,14 +4,14 @@ const FILE_NAME = path.join(__dirname, '../healthy.txt')
 
 module.exports = {
   writeHealthy: async function () {
-    fs.writeFile(FILE_NAME, 'healthy', { encoding: 'utf8', mode: 0o777 }, (err) => {
+    fs.writeFile(FILE_NAME, 'healthy', { encoding: 'utf8', mode: 0o777 }, function (err) {
       if (err) {
         throw err
       }
     })
   },
   deleteHealthy: async function () {
-    fs.unlink(FILE_NAME, (err) => {
+    fs.unlink(FILE_NAME, function (err) {
       if (err && err.code !== 'ENOENT') {
         throw err
       }
--- test/integration/health-service.test.js
+++ test/integration/health-service.test.js
@@ -12,23 +12,23 @@ describe('Health service', () => {
     }
   })
 
-  // test('writeHealthy creates file', async () => {
-  //   await healthService.writeHealthy()
-  //   const result = fs.existsSync(FILE_NAME)
-  //   expect(result).toBe(true)
-  // })
+  test('writeHealthy creates file', async () => {
+    await healthService.writeHealthy()
+    const result = fs.existsSync(FILE_NAME)
+    expect(result).toBe(true)
+  })
 
-  // test('deleteHealthy deletes file', async () => {
-  //   fs.writeFile(FILE_NAME, 'healthy', { encoding: 'utf8', mode: 0o777 }, async (err) => {
-  //     if (err) {
-  //       console.log(err)
-  //       throw err
-  //     }
-  //     await healthService.deleteHealthy()
-  //     const result = fs.existsSync(FILE_NAME)
-  //     expect(result).toBe(false)
-  //   })
-  // })
+  test('deleteHealthy deletes file', async () => {
+    fs.writeFile(FILE_NAME, 'healthy', { encoding: 'utf8', mode: 0o777 }, async function (err) {
+      if (err) {
+        console.log(err)
+        throw err
+      }
+      await healthService.deleteHealthy()
+      const result = fs.existsSync(FILE_NAME)
+      expect(result).toBe(false)
+    })
+  })
 
   test('deleteHealthy does not error if file does not exits', async () => {
     expect(async () => healthService.deleteHealthy()).not.toThrow()
