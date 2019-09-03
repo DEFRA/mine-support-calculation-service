e374fcc1fbacec2cea4fc612c02621b182b7d8d1
John Watson <john.watson2@rpa.gov.uk>
2019-09-03 14:44:33 +0100

    Test removal of create file test

--- test/integration/health-service.test.js
+++ test/integration/health-service.test.js
@@ -18,17 +18,17 @@ describe('Health service', () => {
     expect(result).toBe(true)
   })
 
-  test('deleteHealthy deletes file', async () => {
-    fs.writeFile(FILE_NAME, 'healthy', { encoding: 'utf8', mode: 0o777 }, async (err) => {
-      if (err) {
-        console.log(err)
-        throw err
-      }
-      await healthService.deleteHealthy()
-      const result = fs.existsSync(FILE_NAME)
-      expect(result).toBe(false)
-    })
-  })
+  // test('deleteHealthy deletes file', async () => {
+  //   fs.writeFile(FILE_NAME, 'healthy', { encoding: 'utf8', mode: 0o777 }, async (err) => {
+  //     if (err) {
+  //       console.log(err)
+  //       throw err
+  //     }
+  //     await healthService.deleteHealthy()
+  //     const result = fs.existsSync(FILE_NAME)
+  //     expect(result).toBe(false)
+  //   })
+  // })
 
   test('deleteHealthy does not error if file does not exits', async () => {
     expect(async () => healthService.deleteHealthy()).not.toThrow()
