30ddb032a5fa5ff535996f529964753bbae7f1df
John Watson <john.watson2@rpa.gov.uk>
2019-09-11 08:00:13 +0100

    Console log added for healthz creation

--- server/services/health-service.js
+++ server/services/health-service.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 async function writeLiveness () {
   fs.writeFile('/tmp/calculation-service-healthz.txt', Date.now(), (err) => {
     if (err) throw err
+    console.log('creating healthz file')
   })
 }
 
