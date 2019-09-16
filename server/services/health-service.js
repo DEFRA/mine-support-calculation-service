f6b1fb2b11b8bf53265be738cf9a0805c3308ad0
John Watson <john.watson2@rpa.gov.uk>
2019-09-16 15:22:43 +0100

    Liveness probe unit tests added

--- server/services/health-service.js
+++ server/services/health-service.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 async function writeLiveness () {
   fs.writeFile('/tmp/calculation-service-healthz.txt', Date.now(), (err) => {
     if (err) throw err
-    console.log('creating healthz file')
   })
 }
 
