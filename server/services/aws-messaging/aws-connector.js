152384560c580c82913fd0f81778f9d75899285b
Phil Benson <phil.benson@defra.gov.uk>
2019-12-16 16:11:05 +0000

    Added aws connector, allowing queues to be subscribed to

---
+++ server/services/aws-messaging/aws-connector.js
@@ -0,0 +1,81 @@
+const AWS = require('aws-sdk')
+let promisesDependencySet = false
+let SQSInstance = false
+
+const QUEUE_ARNS = Object.freeze({
+  PAYMENT: 'payment-queue-arn',
+  CALCULATION: 'calculation-queue-arn'
+})
+
+const awsRegion = 'eu-west-2'
+
+const initialiseAWS = () => {
+  if (AWS.config.region !== awsRegion) {
+    AWS.config.update({ region: awsRegion })
+  }
+  if (!promisesDependencySet) {
+    AWS.config.setPromisesDependency(Promise)
+    promisesDependencySet = true
+  }
+}
+
+const createSQS = () => {
+  if (SQSInstance === false) {
+    initialiseAWS()
+    SQSInstance = new AWS.SQS()
+  }
+  return SQSInstance
+}
+
+const resetSQS = () => {
+  SQSInstance = false
+  promisesDependencySet = false
+}
+
+const getQueueUrl = async queueArn => {
+  const { QueueUrl } = await createSQS().getQueueUrl({ QueueName: queueArn }).promise()
+  return QueueUrl
+}
+
+const subscribeToQueue = async (queueArn, callback) => {
+  // var queueURL = "SQS_QUEUE_URL";
+
+  // var params = {
+  //  AttributeNames: [
+  //     "SentTimestamp"
+  //  ],
+  //  MaxNumberOfMessages: 10,
+  //  MessageAttributeNames: [
+  //     "All"
+  //  ],
+  //  QueueUrl: queueURL,
+  //  VisibilityTimeout: 20,
+  //  WaitTimeSeconds: 0
+  // };
+
+  // sqs.receiveMessage(params, function(err, data) {
+  //   if (err) {
+  //     console.log("Receive Error", err);
+  //   } else if (data.Messages) {
+  //     var deleteParams = {
+  //       QueueUrl: queueURL,
+  //       ReceiptHandle: data.Messages[0].ReceiptHandle
+  //     };
+  //     sqs.deleteMessage(deleteParams, function(err, data) {
+  //       if (err) {
+  //         console.log("Delete Error", err);
+  //       } else {
+  //         console.log("Message Deleted", data);
+  //       }
+  //     });
+  //   }
+  // })
+  const QueueName = await getQueueUrl(queueArn)
+  createSQS().receiveMessage({ MaxNumberOfMessages: 1, QueueName }, callback)
+}
+
+module.exports = {
+  createSQS,
+  resetSQS,
+  subscribeToQueue
+}
---
+++ test/unit/aws-messaging/aws-connector.test.js
@@ -0,0 +1,99 @@
+const { createSQS, subscribeToQueue, resetSQS } = require('../../../server/services/aws-messaging/aws-connector')
+const AWS = require('aws-sdk')
+const mockSQSInstances = [] // mock prefix on name is important, so Jest will allow it to be used
+
+jest.mock('aws-sdk', () => ({
+  config: {
+    region: '',
+    setPromisesDependency: jest.fn(),
+    update: jest.fn()
+  },
+  SQS: jest.fn(function () {
+    mockSQSInstances.push(this)
+    this.deleteMessage = jest.fn()
+    this.getQueueUrl = jest.fn(({ QueueName }) => ({
+      promise: jest.fn(() => Promise.resolve({
+        // these have to be hard-coded due to the way jest mocks things...
+        QueueUrl: QueueName === 'calculation-queue-arn'
+          ? 'calculation-queue-url' : 'payment-queue-url'
+      }))
+    }))
+    this.receiveMessage = jest.fn()
+  })
+}))
+
+describe('aws-connector tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    mockSQSInstances.length = 0
+    resetSQS()
+  })
+
+  it('sets the AWS sdk to use promises', () => {
+    createSQS()
+    expect(AWS.config.setPromisesDependency).toHaveBeenCalledWith(Promise)
+  })
+
+  it('sets the region the first time we create an SQS object', () => {
+    createSQS()
+    expect(AWS.config.update).toHaveBeenCalledWith(
+      expect.objectContaining({ region: 'eu-west-2' })
+    )
+  })
+
+  it('only updates the region if it isn\'t the expected value', () => {
+    createSQS()
+    AWS.config.region = 'eu-west-2'
+    createSQS()
+    expect(AWS.config.update).toHaveBeenCalledTimes(1)
+  })
+
+  it('returns expected instance', () => {
+    const sqs = createSQS()
+    expect(sqs instanceof AWS.SQS).toBeTruthy()
+  })
+
+  it('only instantiates one instance of SQS for multiple calls to subscribeToQueue', () => {
+    for (let x = 0; x < 10; x++) {
+      subscribeToQueue('calculation-queue-arn')
+    }
+    expect(mockSQSInstances.length).toBe(1)
+  })
+
+  it('subscribes to queue with correct url', async () => {
+    const testCases = [
+      { queueArn: 'calculation-queue-arn', queueUrl: 'calculation-queue-url' },
+      { queueArn: 'payment-queue-arn', queueUrl: 'payment-queue-url' }
+    ]
+    for (const testCase of testCases) {
+      const { queueArn, queueUrl } = testCase
+      await subscribeToQueue(queueArn, () => {})
+      const [SQSInst] = mockSQSInstances
+      expect(SQSInst.receiveMessage).toHaveBeenCalledWith(
+        expect.objectContaining({
+          QueueName: queueUrl
+        }),
+        expect.any(Function)
+      )
+    }
+  })
+
+  it('sets max messages to 1 when subscribing to queue', async () => {
+    await subscribeToQueue('calculation-queue-arn', () => {})
+    const [SQSInst] = mockSQSInstances
+    expect(SQSInst.receiveMessage).toHaveBeenCalledWith(
+      expect.objectContaining({
+        MaxNumberOfMessages: 1
+      }),
+      expect.any(Function)
+    )
+  })
+
+  it('calls callback when a message is received', async () => {
+    const callback = jest.fn()
+    await subscribeToQueue('calculation-queue-arn', callback)
+    const [SQSInst] = mockSQSInstances
+    SQSInst.receiveMessage.mock.calls[0][1]()
+    expect(callback).toHaveBeenCalled()
+  })
+})
