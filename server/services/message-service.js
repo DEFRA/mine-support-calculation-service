efa36e40c004b5ab9a69b91b6f740599677d35d8
Phil Benson <phil.benson@defra.gov.uk>
2019-12-21 18:40:37 +0000

    Amended message service to subscribe to SQS calculation message queue

--- server/services/message-service.js
+++ server/services/message-service.js
@@ -2,13 +2,31 @@ const MessageSender = require('./messaging/message-sender')
 const MessageReceiver = require('./messaging/message-receiver')
 const messageAction = require('./message-action')
 const config = require('../config')
+const { SqsConsumerFactory } = require('./sqs-messaging/sqs-consumer-factory')
 
 const messageSender = new MessageSender('payment-queue-sender', config.paymentQueueConfig)
 const messageReceiver = new MessageReceiver('calculation-queue-receiver', config.calculationQueueConfig)
 
 async function registerQueues () {
+  const {
+    sqsCalculationQueueConfig: {
+      url,
+      listenCredentials: { accessKeyId, secretAccessKey }
+    }
+  } = config
   await openConnections()
   await messageReceiver.setupReceiver((message) => messageAction(message, messageSender))
+  console.log('setting up sqs consumer')
+  const sqsConsumer = SqsConsumerFactory.create({
+    accessKeyId,
+    handleMessage: message => {
+      console.log('received a message!', message)
+    },
+    secretAccessKey,
+    url
+  })
+  sqsConsumer.start()
+  console.log('consumer polling started')
 }
 
 process.on('SIGTERM', async function () {
---
+++ test/unit/message-service.test.js
@@ -0,0 +1,63 @@
+const mockConfig = {
+  sqsCalculationQueueConfig: {
+    url: 'calculation-queue-url',
+    listenCredentials: {
+      accessKeyId: 'abc-123',
+      secretAccessKey: 'zyx-098'
+    }
+  }
+}
+require('../../server/config')
+require('../../server/services/messaging/message-sender')
+require('../../server/services/messaging/message-receiver')
+const { registerQueues } = require('../../server/services/message-service')
+const { SqsConsumerFactory } = require('../../server/services/sqs-messaging/sqs-consumer-factory')
+
+jest.mock('../../server/services/messaging/message-sender', () => jest.fn(() => ({
+  openConnection: jest.fn().mockImplementation(() => Promise.resolve())
+})))
+jest.mock('../../server/services/messaging/message-receiver', () => jest.fn(() => ({
+  openConnection: jest.fn(() => Promise.resolve()),
+  setupReceiver: jest.fn(() => Promise.resolve())
+})))
+jest.mock('../../server/services/sqs-messaging/sqs-consumer-factory', () => ({
+  SqsConsumerFactory: {
+    create: jest.fn(() => ({
+      start: jest.fn()
+    }))
+  }
+}))
+jest.mock('../../server/config', () => mockConfig)
+
+describe('message-service tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('creates consumer', async () => {
+    await registerQueues()
+    const {
+      sqsCalculationQueueConfig: {
+        url,
+        listenCredentials: { accessKeyId, secretAccessKey }
+      }
+    } = mockConfig
+    expect(SqsConsumerFactory.create).toHaveBeenCalledWith(
+      expect.objectContaining({ url, accessKeyId, secretAccessKey })
+    )
+  })
+
+  test('starts polling', async () => {
+    await registerQueues()
+    expect(SqsConsumerFactory.create.mock.results[0].value.start).toHaveBeenCalled()
+  })
+
+  test('passes message hander', async () => {
+    await registerQueues()
+    expect(SqsConsumerFactory.create).toHaveBeenCalledWith(
+      expect.objectContaining({
+        handleMessage: expect.any(Function)
+      })
+    )
+  })
+})
