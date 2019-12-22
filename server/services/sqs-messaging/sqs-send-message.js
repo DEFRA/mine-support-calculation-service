b30c89606c2791270240390ed7debcf5307322ab
Phil Benson <phil.benson@defra.gov.uk>
2019-12-22 14:20:44 +0000

    FIrst pass on sendMessage

---
+++ server/services/sqs-messaging/sqs-send-message.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk')
+
+const getInitialisationAttributes = config => ({
+  accessKeyId: config.accessKeyId,
+  region: 'eu-west-2',
+  secretAccessKey: config.secretAccessKey
+})
+const getMessageAttributes = config => ({
+  QueueUrl: config.queueUrl,
+  MessageBody: config.messageBody
+})
+
+const sendMessage = async (config) => {
+  const sqs = new AWS.SQS(getInitialisationAttributes(config))
+  sqs.sendMessage(getMessageAttributes(config))
+}
+
+module.exports = { sendMessage }
