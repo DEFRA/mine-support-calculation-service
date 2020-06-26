97f20738a4ff3804cbc7492bd93cbb70641e0533
John Watson <johnwatson484@hotmail.com>
2020-06-26 10:30:10 +0100

    Refactored rounding

--- server/services/round/index.js
+++ server/services/round/index.js
@@ -3,7 +3,7 @@ function round (value, precision) {
   // the approach to avoid that issue applied here is to represent numbers using exponentail notation
   // then round the exponential value, add the negative exponential of the precision
   // and finally convert the exponential value to a number
-  const expValue = `${value}e${precision}`
+  const expValue = Number(`${value}e${precision}`)
   const negativeExpPrecision = `e-${precision}`
   const expResult = Math.round(expValue) + negativeExpPrecision
   return Number(expResult)
