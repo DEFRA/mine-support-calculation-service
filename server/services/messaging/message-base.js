ca78340fba3de7a2d5b984f4ad80316c8dfa1490
Paul Andrews <paul.andrews@defra.gov.uk>
2020-07-30 10:27:13 +0100

    Simpler Jenkins and logging

--- server/services/messaging/message-base.js
+++ server/services/messaging/message-base.js
@@ -3,8 +3,11 @@ const { ServiceBusClient } = require('@azure/service-bus')
 class MessageBase {
   constructor (name, config, credentials) {
     this.name = name
+    console.log('In Message Base')
     this.sbClient = credentials ? ServiceBusClient.createFromAadTokenCredentials(config.host, credentials) : ServiceBusClient.createFromConnectionString(`Endpoint=sb://${config.host}/;SharedAccessKeyName=${config.username};SharedAccessKey=${config.password}`)
+    console.log('Created sbClient')
     this.queueClient = this.sbClient.createQueueClient(config.address)
+    console.log('Created queueClient')
   }
 
   async closeConnection () {
