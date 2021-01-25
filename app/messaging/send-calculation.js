bcb84c3f6e149eca4792ee430dc4460ad161c817
John Watson <johnwatson484@hotmail.com>
2021-01-25 16:17:35 +0000

    Update to use new module

--- app/messaging/send-calculation.js
+++ app/messaging/send-calculation.js
@@ -1,7 +1,10 @@
+const config = require('../config')
+const { MessageSender } = require('ffc-messaging')
 const createMessage = require('./create-message')
 
-async function sendCalcuation (payment, paymentSender) {
+async function sendCalcuation (payment) {
   const message = createMessage(payment)
+  const paymentSender = new MessageSender(config.paymentTopicConfig)
   await paymentSender.sendMessage(message)
   console.info(`Published payment for ${payment.claimId}`)
 }
