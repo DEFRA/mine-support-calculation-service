f1a13cbc19c2b027d72fb79b6bf18bb1da84dbfb
Jeevan Kuduva Ravindran <jeevan.kuduva-ravindran@capgemini.com>
2023-11-16 11:05:45 +0000

    npm lint standard fix

--- test/unit/messaging/index.test.js
+++ test/unit/messaging/index.test.js
@@ -1,20 +1,20 @@
 const { start, stop } = require('../../../app/messaging/index')
 
 jest.mock('../../../app/messaging/index', () => {
-    return {
-        start: jest.fn().mockImplementation(() => 'start receiving message'),
-        stop: jest.fn().mockImplementation(() => 'close connection')
-    }
+  return {
+    start: jest.fn().mockImplementation(() => 'start receiving message'),
+    stop: jest.fn().mockImplementation(() => 'close connection')
+  }
 })
 describe('messaging index', () => {
-    test('start should be called', () => {
-        start()
-        expect(start).toHaveBeenCalled()
-        expect(start()).toBe('start receiving message')
-    })
-    test('stop should be called', () => {
-        stop()
-        expect(stop).toHaveBeenCalled()
-        expect(stop()).toBe('close connection')
-    })
+  test('start should be called', () => {
+    start()
+    expect(start).toHaveBeenCalled()
+    expect(start()).toBe('start receiving message')
+  })
+  test('stop should be called', () => {
+    stop()
+    expect(stop).toHaveBeenCalled()
+    expect(stop()).toBe('close connection')
+  })
 })
